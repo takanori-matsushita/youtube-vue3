85695d12a0b94df4c62ac293cb3c9003c5cc4a93
takanori-matsushita <tknr2899@gmail.com>
2023-01-08 22:26:33 +0900

    add composable useLogin

---
+++ src/composables/useLogin/index.ts
@@ -0,0 +1,59 @@
+import { useRouter } from "vue-router"
+import { FireSignupType } from "@/utils/Firebase/signup"
+import { login as fireLogin } from "@/utils/Firebase/login"
+import { SetErrorFn, useAuthHelper } from "../useAuthHelper"
+import { provideApolloClient, apolloClient } from "@/plugins/apolloClient"
+
+provideApolloClient(apolloClient)
+
+export const useLogin = (props: FireSignupType) => {
+
+  const router = useRouter()
+
+  const formValidation = (setError: SetErrorFn) => {
+    let invalidValidation = false;
+
+    // Emailフォームのバリデーションチェック
+    // 今回はシンプルにするために、入力が空でないかだけ確認する
+    if (!props.email) {
+      setError("email", "メールアドレスを入力してください。");
+      invalidValidation = true;
+    }
+
+    // Passwordフォームのバリデーションチェック
+    // 今回はシンプルにするために、入力が空でないかだけ確認する
+    if (!props.password) {
+      setError("password", "パスワードを入力してください。");
+      invalidValidation = true;
+    }
+
+    // バリデーションが有効か無効化を返す
+    return invalidValidation;
+  };
+
+  // 実際のサインアップのロジック
+  const login = async () => {
+    // Firebaseのサインアップ処理を実行
+    const { user } = await fireLogin({
+      email: props.email || "",
+      password: props.password || "",
+    });
+
+    if (!user?.uid) {
+      throw new Error("ログインに失敗しました。");
+    }
+
+    router.push("/")
+  };
+
+  const { authExecute, error, loading } = useAuthHelper(
+    login,
+    formValidation,
+  );
+
+  return {
+    login: authExecute,
+    error,
+    loading,
+  };
+}
