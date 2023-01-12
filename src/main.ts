a7769c0275af1334fa4f25947e50af5ed2ef3a47
takanori-matsushita <tknr2899@gmail.com>
2023-01-13 00:14:50 +0900

    fix pinia setting

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 import { DefaultApolloClient, apolloClient } from '@/plugins/apolloClient'
+import { createPinia } from 'pinia'
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .use(router)
+  .use(createPinia())
   .provide(DefaultApolloClient, apolloClient)
   .mount('#app')
